35360a2db4fe28fa8a0307d815954eaaeada3ab4
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-01-17 18:14:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# Don't Edi
+# Don't Edit
 
 import os
 
@@ -9,17 +9,24 @@ load_dotenv()
 # Mandatory variables for the bot to start
 API_ID = int(os.getenv("API_ID", "23990433"))
 API_HASH = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6390467508:AAEPjEY6IZbkAHx6LtGx3PYiX8J_A-EtNPY")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("5821871362")] if os.environ.get("ADMINS") else []
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6181493706:AAE4GDBtYO9To_LOO-qSZU79_ABxtE-_M3A")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "nakflixbot")
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://nakflixbot:alpha3720@cluster0.qgybxbu.mongodb.net/?retryWrites=true&w=majority") 
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://sankar:sankar@sankar.lldcdsx.mongodb.net/?retryWrites=true&w=majority") 
 OWNER_ID =  int(os.environ.get("OWNER_ID", "5821871362")) 
-ADMINS.append(5821871362) if OWNER_ID not in ADMINS else []
-ADMINS.append(5821871362)
+ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
+ADMINS.append(1255023013)
+
 #  Optionnal variables
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001870015374")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "False") # For Force Subscription
-BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
-WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
+
+# For Force Subscription
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "SK_MoviesOffl")
+
+# true if forward should be avoided
+BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True")
+
+# image when someone hit /start
+WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '')
 LINK_BYPASS = "False"
